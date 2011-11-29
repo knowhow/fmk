3c751db063fc8b976346e539ba313456578993c6
vsasa <sasa.vranic@bring.out.ba>
2011-11-29 19:42:14 +0100

    tabela rugov, dodatna polja

--- database/misc/db_migrate.sql
+++ database/misc/db_migrate.sql
@@ -2574,6 +2574,10 @@ ALTER TABLE fmk.roba ADD COLUMN k8 character(4);
 ALTER TABLE fmk.roba ADD COLUMN k9 character(4);
 ALTER TABLE fmk.roba ADD COLUMN strings numeric(10,0);
 
+-- alter rugov
+alter table fmk.fakt_rugov add column k1 character(1);
+alter table fmk.fakt_rugov add column k2 character(2);
+alter table fmk.fakt_rugov add column dest character(6);
 
 $$)
 WHERE (u2.knowhow_package_version('fmk') < 30003);
