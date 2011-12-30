28e02dfbf3845c3287ddf0171cb7805aeb89e4bc
vsasa <sasa.vranic@bring.out.ba>
2011-12-30 13:03:26 +0100

    ciscenje semafora od sekvenci

--- database/misc/db_migrate.sql
+++ database/misc/db_migrate.sql
@@ -3450,12 +3450,12 @@ SELECT u2.execute($$
 alter table fmk.semaphores_ld_radn  alter column last_trans_version drop default;   
 alter table fmk.semaphores_ld_ld  alter column last_trans_version drop default;   
 alter table fmk.semaphores_ld_radkr  alter column last_trans_version drop default;   
-alter table fmk.semaphores_ld_dopr  alter column last_trans_version drop default;   
-alter table fmk.semaphores_ld_por  alter column last_trans_version drop default;   
+alter table fmk.semaphores_dopr  alter column last_trans_version drop default;   
+alter table fmk.semaphores_por  alter column last_trans_version drop default;   
 alter table fmk.semaphores_ld_rj  alter column last_trans_version drop default;   
 alter table fmk.semaphores_ld_radsiht  alter column last_trans_version drop default;   
-alter table fmk.semaphores_ld_tippr  alter column last_trans_version drop default;   
-alter table fmk.semaphores_ld_tippr2  alter column last_trans_version drop default;   
+alter table fmk.semaphores_tippr  alter column last_trans_version drop default;   
+alter table fmk.semaphores_tippr2  alter column last_trans_version drop default;   
 alter table fmk.semaphores_ld_norsiht  alter column last_trans_version drop default;   
 alter table fmk.semaphores_ld_obracuni  alter column last_trans_version drop default;   
 alter table fmk.semaphores_ld_parobr  alter column last_trans_version drop default;   
@@ -3463,6 +3463,9 @@ alter table fmk.semaphores_ld_pk_data  alter column last_trans_version drop defa
 alter table fmk.semaphores_ld_pk_radn  alter column last_trans_version drop default;   
 alter table fmk.semaphores_ld_radsat  alter column last_trans_version drop default;   
 alter table fmk.semaphores_ld_tprsiht  alter column last_trans_version drop default;   
+alter table fmk.semaphores_strspr  alter column last_trans_version drop default;   
+alter table fmk.semaphores_vposla  alter column last_trans_version drop default;   
+alter table fmk.semaphores_vprih  alter column last_trans_version drop default;   
 
 $$)
 WHERE (u2.knowhow_package_version('fmk') < 40102);
