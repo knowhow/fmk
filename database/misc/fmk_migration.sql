d5be2426ac8586846b17f4575293851bae29c644
Ernad Husremovic <hernad@bring.out.ba>
2012-12-13 11:55:04 +0100

    add column visak je type

--- database/misc/fmk_migration.sql
+++ database/misc/fmk_migration.sql
@@ -3,7 +3,7 @@
 SELECT u2.execute($$
 ALTER TABLE fmk.fakt_doks ALTER COLUMN oper_id TYPE integer;
 
-ALTER TABLE fmk.fakt_fakt ADD COLUMN idrnal TYPE varchar(10);
+ALTER TABLE fmk.fakt_fakt ADD COLUMN idrnal varchar(10);
 
 $$)
 WHERE (u2.knowhow_package_version('fmk') < 40458);
