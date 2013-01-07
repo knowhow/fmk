de03c908b8e73096374f5eb8b90feb7873dc0133
Ernad Husremovic <hernad@bring.out.ba>
2013-01-07 11:50:48 +0100

    4.7.1

--- database/misc/brojaci_sufix_godina.sql
+++ database/misc/brojaci_sufix_godina.sql
@@ -1,4 +1,3 @@
---- start 4.7.0 verzija
 SELECT u2.execute($$
 
 ALTER TABLE fmk.fakt_fakt  ALTER COLUMN brdok TYPE varchar(12);
@@ -9,3 +8,16 @@ $$)
 WHERE (u2.knowhow_package_version('fmk') < 40700);
 
 --- end 4.7.0 verzija
+
+--- start 4.7.1 verzija
+SELECT u2.execute($$
+
+ALTER TABLE fmk.fin_suban  ALTER COLUMN brdok TYPE varchar(12);
+ALTER TABLE fmk.fin_anal   ALTER COLUMN brdok TYPE varchar(12);
+ALTER TABLE fmk.fin_sint   ALTER COLUMN brdok TYPE varchar(12);
+ALTER TABLE fmk.fin_nalog  ALTER COLUMN brdok TYPE varchar(12);
+
+$$)
+WHERE (u2.knowhow_package_version('fmk') < 40701);
+
+--- end 4.7.1 verzija
